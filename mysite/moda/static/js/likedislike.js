ee6602e4ad047df2716e16840201e7a8f238b6b7
artur <artur.maiap@gmail.com>
2020-09-06 15:18:02 -0300

    action button like/dislike

---
+++ mysite/moda/static/js/likedislike.js
@@ -0,0 +1,35 @@
+
+var listRel = [];
+var listIrre = [];
+
+function addRelevant(path){
+    if(listRel.includes(path)){ }
+    else{
+        if(listIrre.includes(path)){
+            var id = listIrre.indexOf(path);
+            listIrre.splice(id,1);
+            listRel.push(path);
+            alert(listRel);
+        }
+        else{
+            listRel.push(path);
+            alert(listRel);
+        }
+    }
+}
+
+function addIrrelevant(path){
+    if(listIrre.includes(path)){}
+    else{
+        if(listRel.includes(path)){
+            var id = listRel.indexOf(path);
+            listRel.splice(id,1);
+            listIrre.push(path);
+            alert(listIrre);
+        }
+        else{
+            listIrre.push(path);
+            alert(listIrre);
+        }
+    }
+}
