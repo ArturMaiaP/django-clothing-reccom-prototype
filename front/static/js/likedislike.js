519f45ab499cb3c42831d95486168dbae419ee3e
arbezerra <arbezerra@gmail.com>
2022-08-21 22:12:31 -0300

    fix: Images

--- front/static/js/likedislike.js
+++ front/static/js/likedislike.js
@@ -10,34 +10,31 @@ function getRecommendation(target) {
       for (const i of data.img_list) {
           counter++;
         $(target).append(`
-            <div class="col-sm-3 column-padding">
-                <div class="card">
-                        <div class = "card-body">
-                            <div class="row d-flex justify-content-center">
-                                <a data-toggle="modal" data-target="#modal${counter}}">
-                                    <img class="imgIn img-fluid" src= "/static/${i}">
-                                </a>
-                    <!---    <div id="modal${counter}}" class="modal fade">
-                                    <div class="modal-dialog modal-lg" role="content">
-                                        <div class="modal-content">
-                                            <span data-dismiss="modal" class="close text-right">&times;</span>
-                                            <img class="imgZoom align-self-center" src="/static/${i}" >
-                                        </div>
-                                    </div>
-                                </div> -->
-                            </div>
-
-                            <div class="row">
-                                <div class="btn-group">
-                                    <input type="radio" class="btn-check" name="btn${counter}" id="like${counter}" autocomplete="off">
-                                    <label class="btn btn-success" for="like${counter}" onclick="like('${i}');"><span class="fa fa-thumbs-up"></span></label>
-
-                                    <input type="radio" class="btn-check" name="btn${counter}" id="dislike${counter}" autocomplete="off">
-                                    <label class="btn btn-danger" for="dislike${counter}" onclick="dislike('${i}');"><span class="fa fa-thumbs-down"></span></label>
-                                </div>
-                            </div>
-                        </div>
+          <div class="d-flex col-sm-3 column-padding">
+            <div class="card col-12">
+              <div class="card-body">
+                <div class="row">
+                  <a class="d-flex justify-content-center" data-toggle="modal" data-target="#modal${counter}}">
+                    <img class="imgIn img-fluid" src= "/static/${i}">
+                  </a>
+                  <div id="modal${counter}}" class="modal fade">
+                    <div class="modal-dialog modal-lg" role="content">
+                      <div class="modal-content">
+                        <span data-dismiss="modal" class="close text-right">&times;</span>
+                        <img class="imgZoom align-self-center" src="/static/${i}" >
+                      </div>
+                    </div>
+                  </div>
                 </div>
+                <div class="row">
+                  <div class="btn-group">
+                    <input type="radio" class="btn-check" name="btn${counter}" id="like${counter}" autocomplete="off">
+                    <label class="btn btn-success" for="like${counter}" onclick="like('${i}');"><span class="fa fa-thumbs-up"></span></label>
+                    <input type="radio" class="btn-check" name="btn${counter}" id="dislike${counter}" autocomplete="off">
+                    <label class="btn btn-danger" for="dislike${counter}" onclick="dislike('${i}');"><span class="fa fa-thumbs-down"></span></label>
+                  </div>
+                </div>
+              </div>
             </div>`);
       }
     },
