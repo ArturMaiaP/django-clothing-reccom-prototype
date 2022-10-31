a4f2bc8bfd942e28969d002f8ef9933653287306
arbezerra <arbezerra@gmail.com>
2022-10-31 12:27:26 -0300

    fix: Relative path to models

--- api/chatbot/intent.py
+++ api/chatbot/intent.py
@@ -4,7 +4,7 @@ import os
 
 class IntentClassifier:
     def __init__(self):
-        self.data = Data.load('./api/chatbot/model/data.pkl')
-        self.model = pickle.load(open("./api/chatbot/model/model.h5", "rb"))
+        self.data = Data.load(os.path.join(os.path.dirname(__file__), "model/data.pkl"))
+        self.model = pickle.load(open(os.path.join(os.path.dirname(__file__), "model/model.h5"), "rb"))
     def predict(self, text):
         return self.model.predict_proba(self.data.get_bow([text])).tolist()[0]
