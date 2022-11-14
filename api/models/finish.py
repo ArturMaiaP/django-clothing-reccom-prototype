4754bfb75ea8acc74bcb57c4aafcb9c751f2e1c3
arbezerra <arbezerra@gmail.com>
2022-11-14 12:50:13 -0300

    feat: Add finish to db

--- api/models/finish.py
+++ api/models/finish.py
@@ -1,11 +1,10 @@
 from . import db
 from sqlalchemy.sql import func
 
-class Preference(db.Model):
+class Finish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
-    liked = db.Column(db.Boolean)
     
     def __repr__(self):
-        return f'<Preference {self.user_id}>'
+        return f'<Finish {self.user_id},{self.product_id}>'
--- api/recommend.py
+++ api/recommend.py
@@ -2,10 +2,24 @@ from flask import Blueprint, jsonify, request
 import json
 
 from . import login_required, select_images
-from .models import db, Preference, Product, Chat
+from .models import db, Preference, Product, Chat, Finish
 from .recommender.Constantes import *
 
 recommend = Blueprint('recommend', __name__)
+
+@recommend.route('/finish', methods=['POST'])
+@login_required
+def finish(user):
+    product = Product.query.filter_by(name=request.json.get('product')).first()
+    if product:
+        finished = Finish.query.filter_by(user_id = user.id).first()
+        if not finished:
+            finished = Finish(user_id = user.id, product_id = product.id)
+        finished.product_id = product.id
+        db.session.add(finished)
+        db.session.commit()
+        return jsonify({"message": "OK"})
+    return jsonify({"message": "Product invalid."}), 422
 
 @recommend.route('/like', methods=['POST'])
 @login_required
