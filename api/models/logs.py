53c1b089a7546e619dae6050521a7f202b758019
arbezerra <arbezerra@gmail.com>
2022-12-06 12:35:20 -0300

    fix: Logs model

--- api/models/logs.py
+++ api/models/logs.py
@@ -7,7 +7,5 @@ class Logs(db.Model):
     created_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
     
-    preferences = db.relationship("Preference")
-    
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<Logs {self.name}>'
