d086758395351a125d46bec0efd9342b27da5a0e
arbezerra <arbezerra@gmail.com>
2023-01-16 13:09:47 -0300

    feat: Update database

--- process/umap_process.py
+++ process/umap_process.py
@@ -2,39 +2,35 @@
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import umap
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("product.seed.csv")
+encoded_df = pd.get_dummies(df[[
+    "type",
+    "size",
+    "color",
+    "pattern",
+    "fabric",
+]])
 
-data = df[[
-    "a_line",
-    "denim",
-    "dots",
-    "faux",
-    "faux_leather",
-    "Floral",
-    "knit",
-    "Lacy",
-    "leather",
-    "maxi",
-    "midi",
-    "mini",
-    "pencil",
-    "Pleated",
-    "Printed",
-    "skater",
-    "Stripes"
-]].values
 
 # print("Scaling")
 # scaled = StandardScaler().fit_transform(data)
 
 print("Umap")
 reducer = umap.UMAP()
-embedding = reducer.fit_transform(data)
+embedding = reducer.fit_transform(encoded_df.values)
 embedding.shape
 
 
 df['x'] = embedding[:, 0]
 df['y'] = embedding[:, 1]
+
+plt.scatter(
+    embedding[:, 0],
+    embedding[:, 1])
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('UMAP projection', fontsize=24)
+plt.savefig("mygraph.png")
 
 df.to_csv("product.seed.csv")
